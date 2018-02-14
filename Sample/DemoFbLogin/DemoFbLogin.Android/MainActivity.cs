c408ac998ff115cd6341bbd9f216279c4f52f172
Jesus Angulo <jesus.angulo@outlook.com>
2018-02-14 01:15:26 -0500

    Adding sample

---
+++ Sample/DemoFbLogin/DemoFbLogin.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SomosTechies.Facebook;
+using SomosTechies.Facebook.Android;
+using Xamarin.Forms;
+
+namespace DemoFbLogin.Droid
+{
+    [Activity(Label = "DemoFbLogin", Icon = "@drawable/icon", Theme = "@style/MainTheme", 
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            AndroidFacebookManagerService.Init(() => this);
+            DependencyService.Register<AndroidFacebookManagerService>();
+
+            LoadApplication(new App());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            var manager = DependencyService.Get<IFacebookManagerService>();
+            (manager as AndroidFacebookManagerService)?.CallbackManager.OnActivityResult(requestCode, (int)resultCode, data);
+        }
+    }
+}
+
---
+++ Sample/DemoFbLogin/DemoFbLogin/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomosTechies.Facebook;
+using Xamarin.Forms;
+
+namespace DemoFbLogin
+{
+	public partial class MainPage : ContentPage
+	{
+	    private readonly IFacebookManagerService _facebookService;
+
+	    public MainPage()
+		{
+			InitializeComponent();
+		    _facebookService = DependencyService.Get<IFacebookManagerService>();
+		}
+
+	    private async void Login_Clicked(object sender, EventArgs e)
+	    {
+	        var resp = await _facebookService.Login();
+
+	        this.FbStatus.Text = resp.IsCorrect ? 
+	            $"{resp.User.FirstName} {resp.User.LastName} \n {resp.User.Email}" : 
+	            resp.Message;
+	    }
+	}
+}
